485fff79be7bbc775ba8bf59643500b0eca26d0f
Hieu Nguyen <hieunguyen@Hieus-MBP.localdomain>
2024-08-10 19:46:54 -0500

    change Name to className in + button of YourVocabularies.js

--- pages/YourVocabularies.js
+++ pages/YourVocabularies.js
@@ -171,7 +171,7 @@ export default function YourVocabularies() {
                         <div className={` text-sm leading-5 text-gray-900 `}>{listOfDesiredDef[i]}</div>
                         <div className={` text-sm leading-5 text-gray-900 `}>
                             <button
-                                type="submit" Name="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
+                                type="submit" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
                                 onClick={async (e) => {
                                     e.preventDefault();
                                     onIncreaseDef(i);
